01fcfeee55cae927524a88aebec7c03a26640c2d
qkrekfthsus <qkrekfthsus@naver.com>
2021-03-18 21:24:04 +0900

    add redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
+import {Provider} from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+// formik, yup 라이브러리 찾아보기
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<Provider
+		store={createStoreWithMiddleware(Reducer,
+			window.__REDUX_DEVTOOLS_EXTENSION__ &&
+			window.__REDUX_DEVTOOLS_EXTENSION__()
+			)}
+	>
+		<App />
+	</Provider>,
   document.getElementById('root')
 );
 
